e69e612969c19d15fc3a6ece523199389687f773
Ramsay <ramsayleung@gmail.com>
2022-11-13 09:57:48 +0800

    增加练习2-17 和 2-18.

---
+++ chapter2/exercise2-18.scm
@@ -0,0 +1,22 @@
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+
+(define (last-pair items)
+  (list-ref items (- (length items) 1)))
+
+(define (reverse items)
+  (if (null? items)
+      items
+      (append (reverse (cdr items)) (list (car items)))))
