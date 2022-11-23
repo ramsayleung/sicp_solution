a11dd2379eb1e29e1a3c8c1939e5ba89d5f5a2e8
Ramsay <ramsayleung@gmail.com>
2022-11-23 13:51:14 +0800

    增加练习2-47.

---
+++ chapter2/exercise2-46.scm
@@ -0,0 +1,26 @@
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect vec)
+  (car vec))
+
+(define (ycor-vect vec)
+  (cdr vec))
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+	     (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+	     (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect s vec)
+  (make-vect (* (xcor-vect vec) s)
+	     (* (ycor-vect vec) s)))
+
+(define v1 (make-vect 1 2))
+(define v2 (make-vect 3 4))
+(add-vect v1 v2); =>(4 . 6)
+(sub-vect v1 v2); =>(-2 . -2)
+(scale-vect 4 v1); =>(4 . 8)
