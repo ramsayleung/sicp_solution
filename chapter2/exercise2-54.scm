373230b0c836912ad18104b58181cb882a587cd0
Ramsay <ramsayleung@gmail.com>
2022-11-26 20:46:32 +0800

    增加练习2-53 2-54 2-55.

---
+++ chapter2/exercise2-54.scm
@@ -0,0 +1,8 @@
+(define (equal? x1 x2)
+  (if (not (pair? x1))
+      (eq? x1 x2)
+      (and (equal? (car x1) (car x2))
+	   (equal? (cdr x1) (cdr x2)))))
+
+(equal? '(this is a list) '(this is a list)) ;=> #t
+(equal? '(this is a list) '(this (is a) list)) ;=> #f
