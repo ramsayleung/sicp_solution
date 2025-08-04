5cd253689bdfda33f045c7b42e71464656b2bae7
Ramsay Leung <ramsayleung@gmail.com>
2025-08-03 23:05:27 -0700

    Add test for exercise 2-28

--- chapter2/exercise2-28.scm
+++ chapter2/exercise2-28.scm
@@ -3,10 +3,15 @@
   (if (not (pair? items))
       items
       (if (pair? (car items))
-	  (append (fringe (car items)) (fringe (cdr items)))
-	  (append (list (car items)) (fringe (cdr items))))))
+          (append (fringe (car items)) (fringe (cdr items)))
+          (append (list (car items)) (fringe (cdr items))))))
 
-;;;
 (define x (list (list 1 2) (list 3 4)))
-(fringe x) ; => (1 2 3 4)
-(fringe (list x x)) ; => (1 2 3 4 1 2 3 4)
+(module+ test
+  (require rackunit)
+
+  (test-case "Test for fringe"
+             (check-equal? (fringe x) '(1 2 3 4))
+             (check-equal? (fringe (list x x)) '(1 2 3 4 1 2 3 4))
+             )
+  )
