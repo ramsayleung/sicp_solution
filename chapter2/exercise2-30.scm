c4805e47cefad3466e5ab9563d398c34264ad115
Ramsay Leung <ramsayleung@gmail.com>
2025-08-03 23:07:26 -0700

    Add unit test for 2-30

--- chapter2/exercise2-30.scm
+++ chapter2/exercise2-30.scm
@@ -4,17 +4,24 @@
 
 (define (square-tree tree)
   (cond ((null? tree) tree)
-	((not (pair? tree)) (square tree))
-	(else
-	 (cons (square-tree (car tree)) (square-tree (cdr tree))))))
+        ((not (pair? tree)) (square tree))
+        (else
+         (cons (square-tree (car tree)) (square-tree (cdr tree))))))
 
 (define (square-tree-v2 tree)
   (map (lambda (sub-tree)
-	 (if (pair? sub-tree)
-	     (square-tree-v2 sub-tree)
-	     (square sub-tree)))
+         (if (pair? sub-tree)
+             (square-tree-v2 sub-tree)
+             (square sub-tree)))
        tree))
 
 (define x (list 1 (list 2 (list 3 4) 5)))
-(square-tree x) ; => (1 (4 (9 16) 25))
-(square-tree-v2 x); => (1 (4 (9 16) 25))
+
+(module+ test
+  (require rackunit)
+
+  (test-case "Test for square-tree"
+             (check-equal? (square-tree x) '(1 (4 (9 16) 25)))
+             (check-equal? (square-tree-v2 x) '(1 (4 (9 16) 25)))
+             )
+  )
