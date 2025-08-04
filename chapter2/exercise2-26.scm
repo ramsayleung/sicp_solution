a00a36498b1bacbcb397b8cd20bc49a57035d109
Ramsay Leung <ramsayleung@gmail.com>
2025-08-03 23:02:48 -0700

    Add exercise for 2-26

--- chapter2/exercise2-26.scm
+++ chapter2/exercise2-26.scm
@@ -2,6 +2,12 @@
 (define x (list 1 2 3))
 (define y (list 4 5 6))
 
-(append x y); => (1 2 3 4 5 6)
-(cons x y); => ((1 2 3) 4 5 6)
-(list x y); => ((1 2 3) (4 5 6))
+(module+ test
+  (require rackunit)
+
+  (test-case "Test for append, cons, and list"
+             (check-equal? (append x y) '(1 2 3 4 5 6))
+             (check-equal? (cons x y) '((1 2 3) 4 5 6))
+             (check-equal? (list x y) '((1 2 3) (4 5 6)))
+             )
+  )
